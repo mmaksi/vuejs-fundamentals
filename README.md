c5bc35e49e818b8f97869dfb16acc2f69386b0e9
mmaksi <mmaksi.dev@gmail.com>
2023-04-15 15:26:03 +0200

    Learned methods, watchers and computed properties. Also keyboard strokes handling

---
+++ README.md
@@ -0,0 +1,2 @@
+Reactivity is when any changes to the data is immediately reflected on the page.
+
--- app.js
+++ app.js
@@ -1,17 +1,47 @@
 // Define a root component and mount the Vue app into it.
 const vm = Vue.createApp({
-  data() {
+  data() { // This is used to store single values
     return {
       message: "Hello Vue!",
       firstName: "John",
+      middleName: "",
       lastName: "Doe",
+      url: "https://www.google.com",
+      raw_html: "https://www.google.com",
+      age: 20,
     };
   },
-  methods: {
+  methods: { // This is used to store functions
     fullName() {
-      return `${this.firstName.toUpperCase()} ${this.lastName.toUpperCase()}`;
+      // This runs everytime data properties are changed
+      return `${this.firstName.toUpperCase()} ${this.middleName.toUpperCase()} ${this.lastName.toUpperCase()}`;
+    },
+    increment() {
+      return this.age++;
+    },
+    updateLastName(msg, event) {
+      this.lastName = event.target.value;
+      console.log(msg);
+    },
+    updateMiddleName(event) {
+      if (event.key === "h" || event.key === "H") {
+        this.lastName = event.target.value;
+      }
     },
   },
+  computed: { // This is used for functions that calculate a value
+    fullName() {
+      // This runs only when data used here is changed
+      return `${this.firstName.toUpperCase()} ${this.middleName.toUpperCase()} ${this.lastName.toUpperCase()}`;
+    },
+  },
+  watch: { // For async tasks
+    age(newVal, oldVal) {
+      setTimeout(() => {
+        this.age = 20
+      }, 3000);
+    }
+  }
 }).mount("#app");
 
 setTimeout(() => {
